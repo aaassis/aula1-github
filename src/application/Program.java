5d0c69c9433dfd234d3e5fea227ddac4ded0730e
Alexandre de Almeida <ale.assys@gmail.com>
2021-09-17 17:52:13 +0200

    Add msg TEST COMMIT_02

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,8 +4,9 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		System.out.println("TEST!");
-		
+		System.out.println("TEST COMMIT_01");
+		System.out.println("TEST COMMIT_02");
+
 	}
 
 }
